41cc6adfbada7052f24d6032b750bc855f56baeb
Vinicius Marinho <viniciusmarinho1616@gmail.com>
2023-01-29 19:44:16 -0300

    fix: modify type of return of event exec

--- src/types/events.ts
+++ src/types/events.ts
@@ -9,7 +9,7 @@ export interface EventsProps {
 
 export type EventKeys = keyof ClientEvents
 
-export type EventExec<T extends EventKeys> = (props: EventsProps, ...args: ClientEvents[T]) => Awaitable<void>
+export type EventExec<T extends EventKeys> = (props: EventsProps, ...args: ClientEvents[T]) => Awaitable<unknown>
 
 export interface Event<T extends EventKeys> {
   id: T,
